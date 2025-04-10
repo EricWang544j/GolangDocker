66e02cdf69f9986e78be1deaf6a1cb718532d198
tom_glob <544j544j@gmail.com>
2025-04-10 15:51:57 +0800

    mm2

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
-	fmt.Println("Server is running on port 8080...")
+	fmt.Println("Server is running on port 8087...")
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8087", nil))
 }
